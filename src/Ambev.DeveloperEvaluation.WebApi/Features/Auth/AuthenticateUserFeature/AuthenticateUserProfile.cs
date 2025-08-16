084fe1fffc7a01e4167fff5d1341f21facd5862b
Adalberto Rolim Nogueira <betorolim1@gmail.com>
2025-08-16 00:19:51 -0300

    Fix AuthenticateUserResult mapping

--- src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Auth/AuthenticateUserFeature/AuthenticateUserProfile.cs
@@ -16,6 +16,8 @@ public sealed class AuthenticateUserProfile : Profile
     {
         CreateMap<AuthenticateUserRequest, AuthenticateUserCommand>();
 
+        CreateMap<AuthenticateUserResult, AuthenticateUserResponse>();
+
         CreateMap<User, AuthenticateUserResponse>()
             .ForMember(dest => dest.Token, opt => opt.Ignore())
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role.ToString()));
